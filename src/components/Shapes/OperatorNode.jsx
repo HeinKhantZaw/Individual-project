bf63f0b53485b0b02ce01f54ec7583084bb54fb6
heinkhantzaw <hein@engineerforce.io>
2023-10-23 08:57:09 +0100

    add operator node

---
+++ src/components/Shapes/OperatorNode.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {Handle, Position} from "reactflow";
+
+const OperatorNode = ({data}) => {
+    const nodeStyle = {
+        width: 50,
+        height: 20,
+        backgroundColor: "white",
+        color: "black",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: "50px",
+    };
+
+    return (
+        <div style={nodeStyle}>
+            <Handle type="target" position={Position.Top} id={"operator_top"}/>
+            <Handle type="source" position={Position.Left} id="operator_left"/>
+            <p className={"text-xs text-center"}>{data.label}</p>
+            <Handle type="source" position={Position.Right} id={"operator_right"}/>
+            <Handle type="source" position={Position.Bottom} id={"operator_bottom"}/>
+        </div>
+    );
+};
+
+export default OperatorNode;

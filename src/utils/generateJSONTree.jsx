31b3b65a1b21914e666fe0bdbef9654902640311
heinkhantzaw <hein@engineerforce.io>
2023-11-02 17:58:52 +0000

    add util for generating tree

---
+++ src/utils/generateJSONTree.jsx
@@ -0,0 +1,38 @@
+export function generateJSONTree(initialNodes, initialEdges) {
+    const tree = {};
+
+    // Create a map to store nodes by their ID for easy access
+    const nodeMap = new Map();
+    initialNodes.forEach((node) => {
+        nodeMap.set(node.id, node);
+    });
+
+    // Initialize the tree with nodes that don't have any parent (no incoming edges)
+    initialNodes.forEach((node) => {
+        if (!initialEdges.some((edge) => edge.target === node.id)) {
+            tree[node.id] = {...node};
+        }
+    });
+
+    // Recursively build the tree starting from the root nodes
+    const buildTree = (parentId, parent) => {
+        const children = initialEdges
+            .filter((edge) => edge.source === parentId)
+            .map((edge) => {
+                const targetNode = nodeMap.get(edge.target);
+                return {
+                    ...targetNode,
+                    children: buildTree(targetNode.id, targetNode),
+                };
+            });
+
+        return children.length > 0 ? children : null;
+    };
+
+    for (const rootId in tree) {
+        tree[rootId].children = buildTree(rootId, tree[rootId]);
+    }
+
+    // Return the root nodes (usually there's just one root)
+    return Object.values(tree);
+}

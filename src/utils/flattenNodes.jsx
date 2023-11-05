96d79d6f11f444af6d8ab93d895660a7da9852b3
heinkhantzaw <hein@engineerforce.io>
2023-11-05 16:37:43 +0000

    add two util functions

---
+++ src/utils/flattenNodes.jsx
@@ -0,0 +1,24 @@
+export function flattenNodes(initialNodes) {
+    const resultNodes = [];
+    function processNodes(nodes) {
+        for (const node of nodes) {
+            // Add the node to the result
+            resultNodes.push({
+                id: node.id,
+                type: node.type,
+                position: node.position,
+                data: node.data,
+                draggable: node.draggable,
+                conditions: node.conditions,
+            });
+            // Recursively process the children
+            if (node.children) {
+                processNodes(node.children);
+            }
+        }
+    }
+
+    processNodes(initialNodes);
+
+    return resultNodes;
+}
---
+++ src/utils/removeAndFlattenNodes.jsx
@@ -0,0 +1,25 @@
+export function removeAndFlattenNodes(initialNodes, nodesToRemove) {
+    const resultNodes = [];
+    function processNodes(nodes) {
+        for (const node of nodes) {
+            if (!nodesToRemove.includes(node.id)) {
+                // If the node should not be removed, add it to the result
+                resultNodes.push({
+                    id: node.id,
+                    type: node.type,
+                    position: node.position,
+                    data: node.data,
+                    draggable: node.draggable,
+                });
+                // Recursively process the children
+                if (node.children) {
+                    processNodes(node.children);
+                }
+            }
+        }
+    }
+
+    processNodes(initialNodes);
+
+    return resultNodes;
+}

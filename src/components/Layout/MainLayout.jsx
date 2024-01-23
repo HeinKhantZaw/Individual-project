3eb4cb1646b9a5bcf7a57cd5d9a229f29cf0ffcf
heinkhantzaw <hein@engineerforce.io>
2024-01-23 20:12:33 +0000

    update css

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -105,7 +105,7 @@ export default function MainLayout() {
     return (
         <div className="holy-grail">
             <header>
-                <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+                <nav className="border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
                     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                         <a href="/" className="flex items-center">
                             <img src="/assets/logo.png" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
